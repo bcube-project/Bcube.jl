0594ff0d4241864373fec0ba13a90a26b13fa086
mbouyges <maxime.bouyges@onera.fr>
2024-03-13 16:35:09 +0100

    new feature

--- src/cellfunction/meshdata.jl
+++ src/cellfunction/meshdata.jl
@@ -3,7 +3,10 @@ struct CellData <: AbstractMeshDataLocation end
 struct PointData <: AbstractMeshDataLocation end
 
 """
-Represent a data whose values are known at each cell-center (or at each node) of the mesh
+Represent a data whose values are known at each cell-center (or at each node) of the mesh.
+
+Note that the "values" can be anything : an vector of scalar (conductivity by cell), an array
+of functions...
 """
 struct MeshData{L <: AbstractMeshDataLocation, T <: AbstractVector} <: AbstractLazy
     values::T
@@ -16,8 +19,11 @@ set_values!(data::MeshData, values::Union{Number, AbstractVector}) = data.values
 get_location(::MeshData{L}) where {L} = L()
 
 function LazyOperators.materialize(data::MeshData{CellData}, cInfo::CellInfo)
-    PhysicalFunction(x -> get_values(data)[cellindex(cInfo)])
+    PhysicalFunction(x -> _make_callable(get_values(data)[cellindex(cInfo)], x))
 end
+
+_make_callable(f, x) = f(x)
+_make_callable(a::Union{Number, AbstractArray}, x) = a
 
 MeshCellData(values::AbstractVector) = MeshData(CellData(), values)
 MeshPointData(values::AbstractVector) = MeshData(PointData(), values)
