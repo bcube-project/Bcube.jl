98b3035a901bdc401eaa26af2ef19f5075fc1b1e
ghislainb <ghislain.blanchard@gmail.com>
2024-06-16 18:26:39 +0200

    add tests for `Rotation` and `Translation`

--- src/mesh/transformation.jl
+++ src/mesh/transformation.jl
@@ -12,6 +12,21 @@ struct Rotation{T} <: AbstractAffineTransformation
     A::T
 end
 
+"""
+    Rotation(u::AbstractVector, θ::Number)
+
+Return a `Rotation` operator build from a given
+rotation axis `u` and an angle of rotation `θ`.
+
+# Example :
+```julia
+julia> rot = Rotation([0,0,1], π/4);
+julia> x = [3.0,0,0];
+julia> x_rot_ref = 3√(2)/2 .*[1,1,0] # expected result
+julia> all(rot(x) .≈ x_rot_ref)
+true
+```
+"""
 function Rotation(u::AbstractVector, θ::Number)
     Rotation((normalize(u), sincos(θ)))
 end
