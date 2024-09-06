07bce5c44491946661d2a965afb7b5e48b9c0012
lokmanb <bennani.lokman@gmail.com>
2024-09-06 09:27:26 +0200

    adding tests for shape function generator

--- test/interpolation/test_shape_function_generator.jl
+++ test/interpolation/test_shape_function_generator.jl
@@ -70,9 +70,46 @@
         ]
     end
 
-    # TO DO
-    # @testset "Square" begin
-    #     shape = Square()
+    @testset "Square" begin
+        shape = Square()
 
-    # end
+        fs = FunctionSpace(:LagrangeGenerated, 1)
+        λ = ξ -> shape_functions(fs, shape, ξ)
+        ∇λ = ξ -> ∂λξ_∂ξ(fs, shape, ξ)
+        @test λ([-1.0, -1.0]) ≈ [1.0, 0.0, 0.0, 0.0]
+        @test λ([-1.0, 1.0]) ≈ [0.0, 1.0, 0.0, 0.0]
+        @test λ([1.0, -1.0]) ≈ [0.0, 0.0, 1.0, 0.0]
+        @test λ([1.0, 1.0]) ≈ [0.0, 0.0, 0.0, 1.0]
+
+        @test ∇λ([0, 0]) ≈ [
+            -0.25 -0.25
+            -0.25 0.25
+            0.25 -0.25
+            0.25 0.25
+        ]
+
+        m(u, v) = ∫(u ⋅ v)dΩ
+
+        mesh = one_cell_mesh(
+            :quad;
+            xmin = -1.0,
+            xmax = 1.0,
+            ymin = -1.0,
+            ymax = 1.0,
+            zmin = -1.0,
+            zmax = 1.0,
+        )
+        fs = FunctionSpace(:LagrangeGenerated, 1)
+        U = TrialFESpace(fs, mesh)
+        V = TestFESpace(U)
+        dΩ = Measure(CellDomain(mesh), 3)
+        M = assemble_bilinear(m, U, V)
+
+        @test M ≈ (1 / 9) * [
+            4 2 2 1
+            2 4 1 2
+            2 1 4 2
+            1 2 2 4
+        ]
+    end
 end
