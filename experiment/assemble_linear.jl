9bd0662d278a7382272152beb85807671c3d50bc
Maxime Bouyges <maxime.bouyges@gmail.com>
2025-06-26 14:49:21 +0200

    vector assembly on cell is ok

--- experiment/assemble_linear.jl
+++ experiment/assemble_linear.jl
@@ -7,7 +7,7 @@ function kernabs_assemble_linear!(backend, y, f, V, measure, rdhl)
     # @btime assemble_linear_elemental!($idof, $y, $f, $domain, $V, $quadrature, $rdhl)
     # error("dbg")
     #
-    #@device_code_warntype interactive = true  
+    #@device_code_warntype interactive = true
     assemble_linear_kernel!(backend, WORKGROUP_SIZE)(
         y,
         f,
@@ -55,7 +55,9 @@ function assemble_linear_elemental!(
         # fᵥ = Bcube.materialize(_f, eltInfo)
         # value = integrate_on_ref_element(fᵥ, eltInfo, quadrature)
         # b[idof] += value
-        b[idof] += _compute_value(_f, eltInfo, quadrature)
+        x = _compute_value(_f, eltInfo, quadrature)
+        @assert length(x) == 1 "Result is not scalar"
+        b[idof] += first(x)
     end
 end
 function _compute_value(f::F, eltInfo::E, quadrature::Q) where {F, E, Q}
@@ -116,12 +118,16 @@ function run_linear_cell_continuous(backend)
     y = KernelAbstractions.zeros(backend, Float64, get_ndofs(U))
     kernabs_assemble_linear!(backend, y, f, V, dΩ, rdhl)
     display(y)
+    y_from_gpu = Array(y)
 
     # Compare with CPU result
     u_cpu = FEFunction(U_cpu, ones(get_ndofs(U)))
     f_cpu(φ) = ∫(u_cpu * φ)Measure(CellDomain(mesh_cpu), 1)
+    y_cpu = assemble_linear(f_cpu, TestFESpace(U_cpu))
     println("Result on CPU:")
-    display(assemble_linear(f_cpu, TestFESpace(U_cpu)))
+    display(y_cpu)
+
+    @assert y_cpu == y_from_gpu
 
     # CUDA.@device_code_typed interactive = true @cuda cuda_kernel!(res, g, cells, quadrature)
     # @device_code_warntype interactive = true @cuda cuda_kernel!(res, g, cells, quadrature)
@@ -180,12 +186,75 @@ function run_linear_face_discontinuous(backend)
     y = KernelAbstractions.zeros(backend, Float64, get_ndofs(U))
     kernabs_assemble_linear!(backend, y, f, V, dΓ, rdhl)
     display(y)
+    y_from_gpu = Array(y)
 
     # Compare with CPU result
     u_cpu = FEFunction(U_cpu, ones(get_ndofs(U)))
     f_cpu(φ) = ∫(side_n(u_cpu) * jump(φ))Measure(InteriorFaceDomain(mesh_cpu), 1)
+    y_cpu = assemble_linear(f_cpu, TestFESpace(U_cpu))
     println("Result on CPU:")
-    display(assemble_linear(f_cpu, TestFESpace(U_cpu)))
+    display(y_cpu)
+
+    @assert y_cpu == y_from_gpu
+
+    # CUDA.@device_code_typed interactive = true @cuda cuda_kernel!(res, g, cells, quadrature)
+    # @device_code_warntype interactive = true @cuda cuda_kernel!(res, g, cells, quadrature)
+    # @cuda cuda_kernel!(res, g, cells, quadrature)
+end
+
+function run_linear_cell_continuous_vector(backend)
+    # Mesh and domains
+    mesh_cpu = rectangle_mesh(2, 3)
+    mesh = adapt(backend, mesh_cpu)
+    test_arg(backend, mesh)
+    println("mesh on GPU!")
+
+    Ω_cpu = CellDomain(mesh_cpu)
+    Ω = CellDomain(mesh)
+    test_arg(backend, Ω)
+    println("Ω on GPU!")
+
+    dΩ = Measure(Ω, 1)
+    test_arg(backend, dΩ)
+    println("dΩ on GPU!")
+
+    # Build TrialFESpace and TestFESpace
+    U_cpu = TrialFESpace(FunctionSpace(:Lagrange, 1), mesh_cpu; size = 2)
+    U = adapt(backend, U_cpu)
+    test_arg(backend, U)
+    println("U on GPU!")
+
+    V_cpu = TestFESpace(U_cpu)
+    V = TestFESpace(U)
+    test_arg(backend, V)
+    println("V on GPU!")
+
+    # Build ReverseDofHandler
+    rdhl_cpu = ReverseDofHandler(Ω_cpu, V_cpu)
+    rdhl = adapt(backend, rdhl_cpu)
+    test_arg(backend, rdhl)
+    println("rdhl on GPU!")
+
+    # Build FEFunction
+    u = FEFunction(U, KernelAbstractions.ones(backend, Float64, get_ndofs(U)))
+    test_arg(backend, u)
+    println("u on GPU!")
+
+    # Define linear form and assemble
+    f(φ) = u ⋅ φ
+    y = KernelAbstractions.zeros(backend, Float64, get_ndofs(U))
+    kernabs_assemble_linear!(backend, y, f, V, dΩ, rdhl)
+    display(y)
+    y_from_gpu = Array(y)
+
+    # Compare with CPU result
+    u_cpu = FEFunction(U_cpu, ones(get_ndofs(U)))
+    f_cpu(φ) = ∫(u_cpu ⋅ φ)Measure(CellDomain(mesh_cpu), 1)
+    y_cpu = assemble_linear(f_cpu, TestFESpace(U_cpu))
+    println("Result on CPU:")
+    display(y_cpu)
+
+    @assert y_cpu == y_from_gpu
 
     # CUDA.@device_code_typed interactive = true @cuda cuda_kernel!(res, g, cells, quadrature)
     # @device_code_warntype interactive = true @cuda cuda_kernel!(res, g, cells, quadrature)
