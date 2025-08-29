b82b4c116d0ed54675fd2241467002d494a41b71
ghislainb <ghislain.blanchard@gmail.com>
2025-08-29 14:53:01 +0200

    introducing `BcubeBackendCPUSerial`

--- src/backend.jl
+++ src/backend.jl
@@ -1,23 +1,6 @@
-# abstract type AbstractBcubeBackend{T} end
+abstract type AbstractBcubeBackend end
 
-# struct BcubeBackend{T} <: AbstractBcubeBackend{T}
-#     backend::T
-# end
-
-struct BcubeCPUSerial end
+struct BcubeBackendCPUSerial <: AbstractBcubeBackend end
 
 #default:
-get_bcube_backend() = BcubeCPUSerial()
-
-abstract type AbstractBcubeBackendStyle end
-
-struct CPUSerialStyle <: AbstractBcubeBackendStyle end
-backend_style(::BcubeCPUSerial) = CPUSerialStyle()
-
-abstract type AbstractAtomicStyle end
-struct AtomicNeeded <: AbstractAtomicStyle end
-struct AtomicNotNeeded <: AbstractAtomicStyle end
-
-#default:
-isAtomicNeeded(::AbstractBcubeBackendStyle) = AtomicNeeded()
-isAtomicNeeded(::CPUSerialStyle) = AtomicNotNeeded()
+get_bcube_backend() = BcubeBackendCPUSerial()
