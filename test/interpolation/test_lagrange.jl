d02f8698f873ce5a69d5bd1b12619e070646c663
Maxime Bouyges <maxime.bouyges@gmail.com>
2025-10-20 09:19:15 +0200

    v1 ok

--- test/interpolation/test_lagrange.jl
+++ test/interpolation/test_lagrange.jl
@@ -305,9 +305,9 @@ end
         @test λ[8]([0.0, 0.0]) == 0.0
         @test λ[9]([0.0, 0.0]) == 0.0
 
-        @test Bcube.idof_by_volume(FunctionSpace(:Lagrange, 0)) == SA[1]
-        @test Bcube.idof_by_volume(FunctionSpace(:Lagrange, 1)) == SA[]
-        @test Bcube.idof_by_volume(FunctionSpace(:Lagrange, 2)) == SA[5]
+        @test Bcube.idof_by_volume(FunctionSpace(:Lagrange, 0), Square()) == SA[1]
+        @test Bcube.idof_by_volume(FunctionSpace(:Lagrange, 1), Square()) == SA[]
+        @test Bcube.idof_by_volume(FunctionSpace(:Lagrange, 2), Square()) == SA[5]
     end
 
     @testset "Cube" begin
