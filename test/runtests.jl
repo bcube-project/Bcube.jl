db812e2ab7ee298ce89b5dde7d64d4972406c664
mbouyges <maxime.bouyges@onera.fr>
2025-10-22 16:14:54 +0200

    Generated version of _map_over

--- test/runtests.jl
+++ test/runtests.jl
@@ -136,6 +136,7 @@ tempdir = mktempdir()
 
 @testset "Bcube.jl" begin
     custom_include("./test_utils.jl")
+    custom_include("./lazyop/test_lazyop.jl")
     custom_include("./mesh/test_entity.jl")
     custom_include("./mesh/test_connectivity.jl")
     custom_include("./mesh/test_transformation.jl")
