106ae78da2df8d2e14547ffbc9fc2e8b41a13200
mbouyges <maxime.bouyges@onera.fr>
2025-10-23 10:32:11 +0200

    tiny modif

--- test/lazyop/test_lazyop.jl
+++ test/lazyop/test_lazyop.jl
@@ -2,6 +2,6 @@
     args = ((1, 2, 3), (3, 4, 4))
     @test Bcube.LazyOperators._map_over(+, args...) == (4, 6, 7)
 
-    args = (("a", "b", "c", "d"), ("e", "f", "g", "h"))
-    @test Bcube.LazyOperators._map_over(*, args...) == ("ae", "bf", "cg", "dh")
+    args = (("a", "b", 5, "d"), ("e", "f", 4, "h"))
+    @test Bcube.LazyOperators._map_over(*, args...) == ("ae", "bf", 20, "dh")
 end
