bc49184aea9b2c7b8db25a0a2262a59f02dc6319
ghislainb <ghislain.blanchard@gmail.com>
2025-10-26 18:18:09 +0100

    add test for subdomains of periodic and non-periodic `BoundaryFaceDomain`

--- test/mesh/test_domain.jl
+++ test/mesh/test_domain.jl
@@ -123,5 +123,133 @@
                 [265, 266, 269, 271, 273, 275],
             )
         end
+
+        @testset "Periodic BoundaryFaceDomain" begin
+            mesh = read_mesh(
+                joinpath(
+                    @__DIR__,
+                    "..",
+                    "assets",
+                    "rectangle-mesh-tri-quad-nx10-ny10.msh22",
+                );
+                warn = false,
+            )
+            l = 0.5
+            periodicBCType_x =
+                PeriodicBCType(Translation(SA[-2l, 0.0]), ("East",), ("West",))
+            periodicBCType_y =
+                PeriodicBCType(Translation(SA[0.0, 2l]), ("South",), ("North",))
+            Γ_perio_x = BoundaryFaceDomain(mesh, periodicBCType_x)
+            Γ_perio_y = BoundaryFaceDomain(mesh, periodicBCType_y)
+
+            subdomains = Bcube.get_subdomains(Γ_perio_x)
+            @test length(subdomains) == 2
+            @test sum(sdom -> length(Bcube.get_indices(sdom)), subdomains) == 10
+            # subdomain #1
+            @test Bcube.get_elementtype(subdomains[1]) ==
+                  (Bcube.Bar2_t(), Bcube.Tri3_t(), Bcube.Tri3_t())
+            @test length(Bcube.get_indices(subdomains[1])) == 5
+            @test all(Bcube.get_indices(subdomains[1]) .== 1:5)
+            @test all(
+                Bcube.get_cache(Γ_perio_x)[7][Bcube.get_indices(subdomains[1])] .==
+                [106, 120, 123, 149, 151],
+            )
+            @test all(
+                Bcube.get_cache(Γ_perio_x)[8][Bcube.get_indices(subdomains[1])] .==
+                [103, 114, 117, 145, 147],
+            )
+
+            # subdomain #2
+            @test Bcube.get_elementtype(subdomains[2]) ==
+                  (Bcube.Bar2_t(), Bcube.Quad4_t(), Bcube.Quad4_t())
+            @test length(Bcube.get_indices(subdomains[2])) == 5
+            @test all(Bcube.get_indices(subdomains[2])[1:5] .== 6:10)
+            @test all(
+                Bcube.get_cache(Γ_perio_x)[7][Bcube.get_indices(subdomains[2])] .==
+                [268, 270, 272, 274, 276],
+            )
+            @test all(
+                Bcube.get_cache(Γ_perio_x)[8][Bcube.get_indices(subdomains[2])] .==
+                [185, 188, 191, 194, 197],
+            )
+
+            subdomains = Bcube.get_subdomains(Γ_perio_y)
+            @test length(subdomains) == 1
+            @test sum(sdom -> length(Bcube.get_indices(sdom)), subdomains) == 9
+            # subdomain #1
+            @test Bcube.get_elementtype(subdomains[1]) ==
+                  (Bcube.Bar2_t(), Bcube.Quad4_t(), Bcube.Tri3_t())
+            @test length(Bcube.get_indices(subdomains[1])) == 9
+            @test all(Bcube.get_indices(subdomains[1]) .== 1:9)
+            @test all(
+                Bcube.get_cache(Γ_perio_y)[7][Bcube.get_indices(subdomains[1])] .==
+                [39, 46, 63, 77, 80, 82, 92, 132, 138],
+            )
+            @test all(
+                Bcube.get_cache(Γ_perio_y)[8][Bcube.get_indices(subdomains[1])] .==
+                [196, 207, 217, 227, 237, 247, 257, 267, 277],
+            )
+        end
+
+        @testset "BoundaryFaceDomain" begin
+            mesh = read_mesh(
+                joinpath(
+                    @__DIR__,
+                    "..",
+                    "assets",
+                    "rectangle-mesh-tri-quad-nx10-ny10.msh22",
+                );
+                warn = false,
+            )
+            Γs = BoundaryFaceDomain(mesh, "South")
+            Γe = BoundaryFaceDomain(mesh, "East")
+            Γn = BoundaryFaceDomain(mesh, "North")
+            Γw = BoundaryFaceDomain(mesh, "West")
+
+            subdomains = Bcube.get_subdomains(Γs)
+            @test length(subdomains) == 1
+            @test sum(sdom -> length(Bcube.get_indices(sdom)), subdomains) == 9
+            # subdomain #1
+            @test Bcube.get_elementtype(subdomains[1]) == (Bcube.Bar2_t(), Bcube.Tri3_t())
+            @test length(Bcube.get_indices(subdomains[1])) == 9
+            @test all(
+                Bcube.get_indices(subdomains[1]) .== [39, 46, 63, 77, 80, 82, 92, 132, 138],
+            )
+
+            subdomains = Bcube.get_subdomains(Γe)
+            @test length(subdomains) == 2
+            @test sum(sdom -> length(Bcube.get_indices(sdom)), subdomains) == 10
+            # subdomain #1
+            @test Bcube.get_elementtype(subdomains[1]) == (Bcube.Bar2_t(), Bcube.Tri3_t())
+            @test length(Bcube.get_indices(subdomains[1])) == 5
+            @test all(Bcube.get_indices(subdomains[1]) .== [106, 120, 123, 149, 151])
+            # subdomain #2
+            @test Bcube.get_elementtype(subdomains[2]) == (Bcube.Bar2_t(), Bcube.Quad4_t())
+            @test length(Bcube.get_indices(subdomains[2])) == 5
+            @test all(Bcube.get_indices(subdomains[2]) .== [268, 270, 272, 274, 276])
+
+            subdomains = Bcube.get_subdomains(Γn)
+            @test length(subdomains) == 1
+            @test sum(sdom -> length(Bcube.get_indices(sdom)), subdomains) == 9
+            # subdomain #1
+            @test Bcube.get_elementtype(subdomains[1]) == (Bcube.Bar2_t(), Bcube.Quad4_t())
+            @test length(Bcube.get_indices(subdomains[1])) == 9
+            @test all(
+                Bcube.get_indices(subdomains[1]) .==
+                [196, 207, 217, 227, 237, 247, 257, 267, 277],
+            )
+
+            subdomains = Bcube.get_subdomains(Γw)
+            @test length(subdomains) == 2
+            @test sum(sdom -> length(Bcube.get_indices(sdom)), subdomains) == 10
+            # subdomain #1
+            @test Bcube.get_elementtype(subdomains[1]) == (Bcube.Bar2_t(), Bcube.Tri3_t())
+            @test length(Bcube.get_indices(subdomains[1])) == 5
+            @test all(Bcube.get_indices(subdomains[1]) .== [103, 114, 117, 145, 147])
+            # subdomain #2
+            @test Bcube.get_elementtype(subdomains[2]) == (Bcube.Bar2_t(), Bcube.Quad4_t())
+            @test length(Bcube.get_indices(subdomains[2])) == 5
+            @test all(Bcube.get_indices(subdomains[2]) .== [185, 188, 191, 194, 197])
+        end
     end
 end
